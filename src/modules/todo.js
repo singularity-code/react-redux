5dd64598c01cc95e5c5c1a6316e60012451baaa7
DL <neoxrock@gmail.com>
2021-11-19 13:13:47 +1100

    actions

---
+++ src/modules/todo.js
@@ -0,0 +1,30 @@
+// action types
+const CHANGE_INPUT = 'todos/CHANGE_INPUT';
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+// action function
+export const changeInput = (input) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+// there will be 2 items in advance
+let id = 3; // plus 1 when insert called
+export const insert = (text) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
