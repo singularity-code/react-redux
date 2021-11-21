417276641c81122ec5197bf461ee81087106e4a3
DL <neoxrock@gmail.com>
2021-11-22 10:21:35 +1100

    Todos with redux hooks

---
+++ src/containers/TodosContainerWithHooks.js
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useCallback } from 'react';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../components/Todos';
+
+const TodosContainerWithHooks = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
+  const onInsert = useCallback((text) => dispatch(changeInput(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(changeInput(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(changeInput(id)), [dispatch]);
+
+  return <Todos input={input} todos={todos} onChangeInput={onChangeInput} onInsert={onInsert} onToggle={onToggle} onRemove={onRemove}></Todos>;
+};
