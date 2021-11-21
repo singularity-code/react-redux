2c24b6708bfd3710d3d1203c652802f675e2d641
DL <neoxrock@gmail.com>
2021-11-22 10:32:59 +1100

    useActions

--- src/containers/TodosContainerWithHooksActions.js
+++ src/containers/TodosContainerWithHooksActions.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
@@ -8,11 +9,9 @@ const TodosContainerWithHooksActions = () => {
     input: todos.input,
     todos: todos.todos,
   }));
-  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
-    [changeInput, insert, toggle, remove],
-    []
-  )
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions([changeInput, insert, toggle, remove], []);
   return <Todos input={input} todos={todos} onChangeInput={onChangeInput} onInsert={onInsert} onToggle={onToggle} onRemove={onRemove}></Todos>;
 };
 
-export default TodosContainerWithHooksActions;
+// React memo need for optimization when using useSelector
+export default React.memo(TodosContainerWithHooksActions);
