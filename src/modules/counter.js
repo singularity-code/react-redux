18e6b9d72c8cd41d17631a28001f5bd79e07321f
DL <neoxrock@gmail.com>
2021-11-22 09:41:36 +1100

    counter with handle action

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,16 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // action names
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // action functions
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+// action with redux-actions
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // default state
 const initialState = {
@@ -12,19 +18,26 @@ const initialState = {
 };
 
 // reducer
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// reducer with handle actions
+const counter = handleActions({
+  [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+  [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+}, initialState);
+
 // default export only one
 export default counter;
